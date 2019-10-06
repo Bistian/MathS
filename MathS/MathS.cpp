69ad3a80789556aef10ccb7652f9e2fd7b7b1fc6
Sebastian Rodriguez <SPRodriguez@student.fullsail.edu>
2019-10-06 19:34:32 -0400

    Some matrix stuff added

--- MathS/MathS.cpp
+++ MathS/MathS.cpp
@@ -1,10 +1,11 @@
 #include "MathS.h"
-#include "Math.h"
+#include <math.h>
+
+#define PI 3.14159f
 
 //====================================
 //	Vector3 Shit
 //====================================
-#pragma region Vector3
 
 #pragma region Constructors
 //Default Constructor
@@ -13,7 +14,7 @@ MathS::Vec3::Vec3()
 	x = 1.0f;
 	y = 1.0f;
 	z = 1.0f;
-};
+}
 //Set all to same value
 MathS::Vec3::Vec3(float val)
 {
@@ -60,7 +61,7 @@ MathS::Vec3 MathS::Vec3::operator =(const Vec3& vector)
 #pragma region Methods
 float MathS::Vec3::Dot(const Vec3& vector)
 {
-	return x* vector.x + y * vector.y + z * vector.z;
+	return x * vector.x + y * vector.y + z * vector.z;
 }
 
 float MathS::Vec3::Length()
@@ -86,7 +87,7 @@ MathS::Vec3  MathS::Vec3::ScalarMult(float scaler)
 MathS::Vec3  MathS::Vec3::Reflect(Vec3& NormVector)
 {
 	NormVector = NormVector.Normalize();
-	return *this - NormVector.ScalarMult(2 *this ->Dot(NormVector));
+	return *this - NormVector.ScalarMult(2 * this->Dot(NormVector));
 }
 
 MathS::Vec3  MathS::Vec3::Cross(const Vec3& vector)
@@ -104,12 +105,9 @@ MathS::Vec4 MathS::Vec3::Convert4()
 }
 #pragma endregion Methods
 
-#pragma endregion Vector3
-
 //====================================
 //	Vector4 Shit
 //====================================
-#pragma region Vector4
 
 #pragma region Constructors
 //Default Constructor
@@ -199,8 +197,365 @@ MathS::Vec3 MathS::Vec4::Convert3()
 }
 #pragma endregion Methods
 
-#pragma endregion Vector4
+//====================================
+//	Matrix3 Shit
+//====================================
+
+void MathS::Mat3::Fill(float fill)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; i < 3; i++)
+		{
+			values[i][j] = fill;
+		}
+	}
+}
+
+#pragma region Constructors
+//Default Constructor
+MathS::Mat3::Mat3()
+{
+	Fill(1);
+}
+
+//Copy Constructor Matrix
+MathS::Mat3::Mat3(const Mat3& matrix)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			values[i][j] = matrix.values[i][j];
+		}
+	}
+}
+
+//Copy Constructor Vector
+MathS::Mat3::Mat3(std::vector<float>& vector)
+{
+	int count = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			values[i][j] = vector[count];
+			++count;
+		}
+	}
+}
+#pragma endregion Constructors
+
+#pragma region Operator Overloads
+MathS::Mat3 MathS::Mat3::operator+(const Mat3& matrix)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			values[i][j] += matrix.values[i][j];
+		}
+	}
+	return *this;
+}
+
+MathS::Mat3 MathS::Mat3::operator-(const Mat3& matrix)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			values[i][j] -= matrix.values[i][j];
+		}
+	}
+	return *this;
+}
+
+MathS::Mat3 MathS::Mat3::operator*(float number)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			values[i][j] *= number;
+		}
+	}
+	return *this;
+}
+#pragma endregion Operator Overloads
+
+#pragma region Methods
+MathS::Mat3 MathS::Mat3::Identity()
+{
+	Fill(0);
+	values[0][0] = 1.0f;
+	values[1][1] = 1.0f;
+	values[2][2] = 1.0f;
+	return *this;
+}
+
+MathS::Mat3 MathS::Mat3::Zero()
+{
+	Fill(0);
+	return *this;
+}
+
+MathS::Mat3 MathS::Mat3::Transpose()
+{
+	Mat3 matrix;
+	matrix.Fill(0);
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			matrix.values[i][j] = values[j][i];
+		}
+	}
+	return matrix;
+}
+
+float MathS::Mat3::Determinant()
+{
+	return values[0][0] * (values[1][1] * values[2][2] - values[2][1] * values[1][2])
+		- values[0][1] * (values[1][0] * values[2][2] - values[2][0] * values[1][2])
+		+ values[0][2] * (values[1][0] * values[2][1] - values[2][0] * values[1][1]);
+}
+#pragma endregion Methods
 
 //====================================
-//	Matrix Shit
+//	Matrix4 Shit
 //====================================
+
+void MathS::Mat4::Fill(float fill)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; i < 4; i++)
+		{
+			values[i][j] = fill;
+		}
+	}
+}
+
+#pragma region Constructors
+//Default Constructor
+MathS::Mat4::Mat4()
+{
+	Fill(1);
+}
+
+//Copy Constructor Matrix
+MathS::Mat4::Mat4(const Mat4& matrix)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			values[i][j] = matrix.values[i][j];
+		}
+	}
+}
+
+//Copy Constructor Vector
+MathS::Mat4::Mat4(std::vector<float>& vector)
+{
+	int count = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			values[i][j] = vector[count];
+			++count;
+		}
+	}
+}
+#pragma endregion Constructors
+
+#pragma region Operator Overloads
+MathS::Mat4 MathS::Mat4::operator+(const Mat4& matrix)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			values[i][j] += matrix.values[i][j];
+		}
+	}
+	return *this;
+}
+
+MathS::Mat4 MathS::Mat4::operator-(const Mat4& matrix)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			values[i][j] -= matrix.values[i][j];
+		}
+	}
+	return *this;
+}
+
+//Multipy by Scalar
+MathS::Mat4 MathS::Mat4::operator*(float number)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			values[i][j] *= number;
+		}
+	}
+	return *this;
+}
+
+//Multiply By Vec3
+MathS::Mat4 MathS::Mat4::operator*(const Vec3& vector)
+{
+	values[0][0] *= vector.x;	values[0][1] *= vector.y;	values[0][2] *= vector.z;
+	values[1][0] *= vector.x;	values[1][1] *= vector.y;	values[1][2] *= vector.z;
+	values[2][0] *= vector.x;	values[2][1] *= vector.y;	values[2][2] *= vector.z;
+	values[3][0] *= vector.x;	values[3][1] *= vector.y;	values[3][2] *= vector.z;
+	return *this;
+}
+
+//Multiply By Vec4
+MathS::Mat4 MathS::Mat4::operator*(const Vec4& vector)
+{
+	values[0][0] *= vector.x;	values[0][1] *= vector.y;	values[0][2] *= vector.z;	values[0][3] *= vector.w;
+	values[1][0] *= vector.x;	values[1][1] *= vector.y;	values[1][2] *= vector.z;	values[1][3] *= vector.w;
+	values[2][0] *= vector.x;	values[2][1] *= vector.y;	values[2][2] *= vector.z;	values[2][3] *= vector.w;
+	values[3][0] *= vector.x;	values[3][1] *= vector.y;	values[3][2] *= vector.z;	values[3][3] *= vector.w;
+	return *this;
+}
+
+//Multiply 4x4 By 4x4
+MathS::Mat4 MathS::Mat4::operator*(const Mat4& matrix)
+{
+	Mat4 matrix1;
+	matrix1.Fill(0);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				matrix1.values[i][j] += values[i][k] * matrix.values[k][j];
+			}
+		}
+	}
+	return matrix1;
+}
+
+
+#pragma endregion Operator Overloads
+
+#pragma region Methods
+MathS::Mat4 MathS::Mat4::Scale(Vec3 vector)
+{
+	return *this * vector;
+}
+
+MathS::Mat4 MathS::Mat4::Scale(Vec4 vector)
+{
+	return *this * vector;
+}
+
+MathS::Mat4 MathS::Mat4::RotateX(float degree)
+{
+	Mat4 matrix;
+	matrix.Fill(0);
+
+	degree = degree * (PI / 180);
+
+	matrix.values[0][0] = 1;
+	matrix.values[1][1] = cosf(degree);		matrix.values[1][2] = -sin(degree);
+	matrix.values[2][1] = sinf(degree);		matrix.values[2][2] = cosf(degree);
+	matrix.values[3][3] = 1;
+
+	return *this * matrix;
+}
+
+MathS::Mat4 MathS::Mat4::RotateY(float degree)
+{
+	Mat4 matrix;
+	matrix.Fill(0);
+
+	degree = degree * (PI / 180);
+
+	matrix.values[1][1] = 1;
+	matrix.values[0][0] = cosf(degree);		matrix.values[0][2] = sin(degree);
+	matrix.values[2][0] = -sinf(degree);	matrix.values[2][2] = cosf(degree);
+	matrix.values[3][3] = 1;
+
+	return *this * matrix;
+}
+
+MathS::Mat4 MathS::Mat4::RotateZ(float degree)
+{
+	Mat4 matrix;
+	matrix.Fill(0);
+
+	degree = degree * (PI / 180);
+
+	matrix.values[2][2] = 1;
+	matrix.values[0][0] = cosf(degree);		matrix.values[0][1] = -sin(degree);
+	matrix.values[1][0] = sinf(degree);		matrix.values[1][1] = cosf(degree);
+	matrix.values[3][3] = 1;
+
+	return *this * matrix;
+}
+
+MathS::Mat4 MathS::Mat4::Transform(Vec4& vector)
+{
+	Mat4 matrix;
+	matrix.Fill(0);
+
+	matrix.values[0][0] = 1;
+	matrix.values[1][1] = 1;
+	matrix.values[2][2] = 1;
+	matrix.values[3][0] = vector.x;
+	matrix.values[3][1] = vector.y;
+	matrix.values[3][2] = vector.z;
+	matrix.values[3][3] = vector.w;
+
+	return matrix;
+}
+
+MathS::Mat4 MathS::Mat4::Identity()
+{
+	Mat4 matrix;
+	matrix.Fill(0);
+
+	matrix.values[0][0] = 1;
+	matrix.values[1][1] = 1;
+	matrix.values[2][2] = 1;
+	matrix.values[3][3] = 1;
+	return matrix;
+}
+
+MathS::Mat4 MathS::Mat4::Zero()
+{
+	Fill(0.0f);
+	return *this;
+}
+
+MathS::Mat4 MathS::Mat4::Transpose()
+{
+	Mat4 matrix;
+	matrix.Fill(0);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			matrix.values[i][j] = values[j][i];
+		}
+	}
+	return matrix;
+}
+
+float MathS::Mat4::Determinant()
+{
+}
+
+#pragma endregion Methods
--- MathS/MathS.h
+++ MathS/MathS.h
@@ -1,15 +1,15 @@
 #pragma once
+#include <vector>
 
 namespace MathS
 {
-		//Forward Decleration
-		class Vec4;
+	//Forward Decleration
+	class Vec4;
 
 	class Vec3
 	{
 	public:
 		float x, y, z;
-
 
 		//Constructors
 		Vec3();
@@ -57,4 +57,70 @@ namespace MathS
 		Vec4 ScalarMult(float scaler);
 		Vec3 Convert3();
 	};
+
+	class Mat3
+	{
+	private:
+		void Fill(float fill);
+
+	public:
+		float values[3][3];
+
+		//Constructors
+		Mat3();
+		Mat3(const Mat3& matrix);
+		Mat3(std::vector<float>& vector);
+
+		//Operator overrides
+		Mat3 operator +(const Mat3& matrix);
+		Mat3 operator -(const Mat3& matrix);
+		Mat3 operator *(float number);
+
+		//Methods
+		Mat3 Identity();
+		Mat3 Zero();
+		Mat3 Transpose();
+		float Determinant();
+	};
+
+
+	class Mat4
+	{
+	private:
+		void Fill(float fill);
+
+	public:
+		float values[4][4];
+
+		//Constructors
+		Mat4();
+		Mat4(const Mat4& matrix);
+		Mat4(std::vector<float>& vector);
+
+		//Operator overrides
+		Mat4 operator +(const Mat4& matrix);
+		Mat4 operator -(const Mat4& matrix);
+		//Multipy by Scalar
+		Mat4 operator *(float number);
+		//Multiply By Vec3
+		Mat4 operator *(const Vec3& vector);
+		//Multiply By Vec4
+		Mat4 operator *(const Vec4& vector);
+		//Multiply 4x4 By 4x4
+		Mat4 operator *(const Mat4& matrix);
+
+		//Methods
+		Mat4 Scale(Vec3 vector);
+		Mat4 Scale(Vec4 vector);
+		Mat4 RotateX(float degree);
+		Mat4 RotateY(float degree);
+		Mat4 RotateZ(float degree);
+		Mat4 Transform(Vec4& vector);
+		Mat4 Identity();
+		Mat4 Zero();
+		Mat4 Transpose();
+		float Determinant();
+	
+	};
+
 }
